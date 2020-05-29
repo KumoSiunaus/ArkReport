acae00cd7573c367f5000f42627203941f2789b2
Kumo <zhanghaoyun0803@gmail.com>
2020-05-29 21:43:17 +0900

    因ocr库bug修复而更新

--- find.py
+++ find.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 
 import cv2
+from cnocr import ENG_LETTERS, NUMBERS, CnOcr
+
 from resolution import childhwnd_size
-from cnocr import CnOcr, NUMBERS, ENG_LETTERS
 
 ocr = CnOcr()
 
@@ -24,8 +25,7 @@ def image(img, itemimg, loc):
 def quantity(img):
 
     ret, img_bin = cv2.threshold(img, 170, 255, cv2.THRESH_BINARY_INV)
-    cand_alphabet = "0123456789"
-    ocr.alpha(cand_alphabet)
+    ocr.set_cand_alphabet("0123456789")
     res = ocr.ocr_for_single_line(img_bin)
     for i in range(res.count(" ")):
         res.remove(" ")
@@ -37,8 +37,7 @@ def quantity(img):
 def text(img, threshold):
 
     ret, img_bin = cv2.threshold(img, threshold, 255, cv2.THRESH_BINARY_INV)
-    cand_alphabet = None
-    ocr.alpha(cand_alphabet)
+    ocr.set_cand_alphabet(None)
     res = (ocr.ocr_for_single_line(img_bin))
     for i in range(res.count(" ")):
         res.remove(" ")
@@ -52,8 +51,7 @@ def text(img, threshold):
 def stage(img):
 
     ret, img_bin = cv2.threshold(img, 254, 255, cv2.THRESH_BINARY_INV)
-    cand_alphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ-"
-    ocr.alpha(cand_alphabet)
+    ocr.set_cand_alphabet("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ-")
     res = (ocr.ocr_for_single_line(img_bin))
     for i in range(res.count(" ")):
         res.remove(" ")
